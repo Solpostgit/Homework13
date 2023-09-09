8fd5468c3892558bed7286311f0cec58009d88fa
Solpostgit <begemot_off@mail.ru>
2023-09-09 23:24:27 +0300

    Added methods equals(), hashCode in classes Author.

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,21 @@ public class Main {
 
         Book warAndPeace = new Book("War and Peace", 1950, tolstoy);
 
-        Book mciry = new Book("MciryTYTY", 1955, lermontov);
+        Book mciry = new Book("Mciry", 1955, lermontov);
 
         Book aNewBook = new Book("Mciry", 1955, lermontov);
 
-        warAndPeace.setPublishingYear(1951); //изменяем год публикации, через сеттер
-
-        System.out.println("book1 = " + warAndPeace.getPublishingYear());
-
-        System.out.println(warAndPeace);
+        System.out.println(warAndPeace); // вывод toString
         System.out.println(mciry);
 
-        System.out.println(warAndPeace.equals(aNewBook));
+        System.out.println(warAndPeace.equals(aNewBook)); // Сравнение книг
+        System.out.println(tolstoy.equals(lermontov)); // Сравнение авторов
 
-        System.out.println(warAndPeace.hashCode());
-        System.out.println(mciry.hashCode());
-        System.out.println(aNewBook.hashCode());
+        System.out.println(warAndPeace.hashCode()); // хэшКод книги
+        System.out.println(mciry.hashCode()); // одинаковое содержание mciry и aNewBook
+        System.out.println(aNewBook.hashCode()); // одинаковое содержание mciry и aNewBook
+
+        System.out.println(tolstoy.hashCode()); // хэшКод Толстого
+        System.out.println(lermontov.hashCode()); // хэшКод Лермонтова
     }
 }
